e3f69353941c5c823bfe5d8b83f95ef8ed111f05
Tony DiPietri <lodsiek@gmail.com>
2019-04-22 20:24:17 -0500

    Chapter 4 commit

---
+++ listing4-2.go
@@ -0,0 +1,16 @@
+//Listing 4.2
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var count = 0
+
+	for count = 10; count > 0; count-- {
+		fmt.Println(count)
+	}
+	fmt.Println(count)
+}
---
+++ listing4-3.go
@@ -0,0 +1,13 @@
+//Listing 4.3
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	for count := 10; count > 0; count-- {
+		fmt.Println(count)
+	}
+}
