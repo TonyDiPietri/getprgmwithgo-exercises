1a14064db53b7cb391cdbb028206a09ba98b3633
Tony DiPietri <lodsiek@gmail.com>
2019-04-23 22:42:53 -0500

    Lesson 5 work

--- lesson5capstone.go
+++ lesson5capstone.go
@@ -4,13 +4,6 @@ a program on your own. Your challenge is to write a ticket generator in the Go P
 that makes use of variables, constants, switch, if, and for. It should also draw
 on the fmt and math/rand packages to display and align text and to generate random
 numbers.
-When planning a trip to Mars, it would be handy to
-have ticket pricing from multiple spacelines in one
-place. Websites exist that aggregate ticket prices for
-airlines, but so far nothing exists for spacelines.
-That’s not a problem for you, though. You can use
-Go to teach your computer to solve problems like
-this.
 Start by building a prototype that generates 10 random
 tickets and displays them in a tabular format
 with a nice header, as follows:
@@ -19,7 +12,7 @@ The table should have four columns:
  The spaceline company providing the service
  The duration in days for the trip to Mars (one-way)
  Whether the price covers a return trip
- The price in millions of dollars 
+ The price in millions of dollars 
 For each ticket, randomly select one of the following spacelines: Space Adventures,
 SpaceX, or Virgin Galactic.
 Use October 13, 2020 as the departure date for all tickets. Mars will be 62,100,000 km
@@ -27,9 +20,6 @@ away from Earth at the time.
 Randomly choose the speed the ship will travel, from 16 to 30 km/s. This will determine
 the duration for the trip to Mars and also the ticket price. Make faster ships more expensive,
 ranging in price from $36 million to $50 million. Double the price for round trips.
-When you’re done, post your solution to the Get Programming with Go forums at
-forums.manning.com/forums/get-programming-with-go. If you get stuck, feel free to
-ask questions on the forums, or take a peek at the appendix for our solution.
 */
 package main
 
@@ -38,6 +28,38 @@ import (
 	"math/rand"
 )
 
-func main {
-	
+const distance = 62100000
+
+func main() {
+
+	var company, days, returnyn, price int
+
+	fmt.Printf("%-16v %v %v %v\n", "Spaceline", "Days", "Trip type", "Price")
+	fmt.Printf("=====================================\n")
+
+	for count := 0; count < 10; count++ {
+
+		var compname string
+
+		//randomly choose a company
+		company = rand.Intn(3)
+
+		//Set string 'compname' based on outcome of random int 'company'
+		if company == 0 {
+			compname = "Space Adventures"
+		} else if company == 1 {
+			compname = "SpaceX"
+		} else if company == 2 {
+			compname = "Virgin Galactic"
+		}
+
+		//Determine days for each ticket based on random speed and 'distance'
+		speed := (rand.Intn(14) + 16)
+		days = (distance / (speed * 86400)) //86400 converts 16-30 km/s into km/day
+		price = 36 + (speed - 16) //starts price at 36 and increases as speed rises
+
+		fmt.Printf("%-16v %4v %9v $%2v\n", compname, days, returnyn, price)
+
+	}
+
 }
